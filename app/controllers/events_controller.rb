1845b10d70aff2e052af72d5114de623aa5d043f
Sofia <jonssonsofia93@gmail.com>
2019-05-24 11:49:50 -0700

    Created backend of app. Created, Migrated, & Seeded the Db. Populated App Controllers and Models. Made sure routes were established.

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,38 @@
+class EventsController < ApplicationController
+	before_action :set_params, only: [:update, :destroy]
+	def index
+		@events = Event.all
+		render json: @events
+	end
+
+	def create
+		@event = Event.create(event_params)
+		render json: @event
+	end
+
+	def update
+		@event.update(event_params)
+			if @event.save
+				render json: @event, status: :accepted
+			else
+				render json: { errors: @event.errors.full_messages }, status: :unprocessible_entity
+			end
+	end
+
+	def destroy
+		puts @event
+			@event.destroy
+	end
+
+
+	private
+
+	def event_params
+		params.require(:event).permit!
+	end
+
+	def set_params
+		@event = Event.find(params[:id])
+	end
+
+end

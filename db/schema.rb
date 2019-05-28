68acb571b5f18384910199d1ed7262b766f0153b
Sofia <jonssonsofia93@gmail.com>
2019-05-28 11:40:46 -0700

    Added password attribute to Users Model and seeded data

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_05_24_183139) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

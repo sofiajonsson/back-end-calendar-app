d7f25e9b625d4f4d86a1df47e32183dba11e8bc4
Sofia <jonssonsofia93@gmail.com>
2019-05-28 13:41:45 -0700

    Added JWT from Steve's Lecture

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,22 @@
 class UsersController < ApplicationController
-	def index
-		@users = User.all
-		render json: @users
-	end
+	skip_before_action :authorized, only: [:create]
+
+	  def profile
+	    render json: { user: UserSerializer.new(current_user) }, status: :accepted
+	  end
 
 	def create
-		@user = User.create(user_params)
-		render json: @user
-	end
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: { user: UserSerializer.new(@user) }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+  end
 
 	private
 
 	def user_params
-		params.require(:user).permit!
+		params.require(:user).permit(:name, :email, :password)
 	end
 end
